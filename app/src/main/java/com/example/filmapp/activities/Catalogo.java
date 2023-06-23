a07284e4bf4f134f77a98b0c52e1281a43745d4d
unknown <gasparettocaim@gmail.com>
2023-06-23 17:55:16 -0400

    Modificar arquivos Catalogo.java e activity_catalogo.xml

--- app/src/main/java/com/example/filmapp/activities/Catalogo.java
+++ app/src/main/java/com/example/filmapp/activities/Catalogo.java
@@ -1,16 +1,44 @@
 package com.example.filmapp.activities;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
-import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.filmapp.R;
 
-public class Catalogo extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class Catalogo extends RecyclerView.Adapter<Catalogo.FilmeViewHolder> {
+    private ArrayList<Object> listaFilmes;
+
+    public Catalogo(ArrayList<Object> listaFilmes) {
+        this.listaFilmes = listaFilmes;
+    }
+
+    @NonNull
+    @Override
+    public FilmeViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_catalogo, parent, false);
+        return new FilmeViewHolder(view);
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_catalogo);
+    public void onBindViewHolder(@NonNull FilmeViewHolder holder, int position) {
+        // Implementação do onBind para atualizar os dados do item do RecyclerView
+    }
+
+    @Override
+    public int getItemCount() {
+        return listaFilmes.size();
+    }
+
+    public static class FilmeViewHolder extends RecyclerView.ViewHolder {
+        public FilmeViewHolder(@NonNull View itemView) {
+            super(itemView);
+            // Inicializar os componentes do item do RecyclerView aqui
+        }
     }
 }
